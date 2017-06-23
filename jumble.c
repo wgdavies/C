aef2108fcc750a871b085084ebaf7914bcb46d3c
Walter Davies <Walter.Davies1@aexp.com>
2017-06-23 15:52:27 -0700

    compiles without errors, just does not yet run without errors

--- jumble.c
+++ jumble.c
@@ -3,12 +3,23 @@
 // this is about the most inefficient implementation possible
 // partly to just get a POC running
 // and partly to see how much better we can make it!
+//
+// features and command-line options:
+//  - override max file/dictionary size (default 10MiB)
+//  - set number of matches to return (default 1, 0 returns all matches)
+//  - return partial string matches (e.g 'sprain' also matches 'unsprained')
+//
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
 
-#define dictionary "/usr/share/dict/words"
+#define DICTIONARY "/usr/share/dict/words"
+#define MAX_DICT_SIZE 10485760
+#define handle_error(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
 int fileSearch(char *fname, char *str) {
     FILE *fp;
@@ -53,19 +64,22 @@ void swap(char *x, char *y)
    This function takes three parameters:
    1. String
    2. Starting index of the string
-   3. Ending index of the string. */
-void permute(char *a, int l, int r)
+   3. Ending index of the string.
+   4. pointer to memory-mapped DICTIONARY file
+*/
+void permute(char *a, int l, int r, const void *dict, size_t strsz)
 {
    int i;
    if (l == r)
        // printf("%s\n", a);
-       fileSearch(dictionary, a);
+       // fileSearch(DICTIONARY, a);
+       memmem(dict, MAX_DICT_SIZE, a, strsz);
    else
    {
        for (i = l; i <= r; i++)
        {
           swap((a+l), (a+i));
-          permute(a, l+1, r);
+          permute(a, l+1, r, DICTIONARY, strsz);
           swap((a+l), (a+i)); //backtrack
        }
    }
@@ -78,18 +92,35 @@ int main(int argc, char *argv[])
 	printf("usage: %s %s\n", argv[0], "<word>");
 	exit(1);
     }
-
+    // replace this crap with getopt(), popt(), &c
     char *str = argv[1];
-    // generate list of permutations
-    // e.g char str[] = "asprin";
     int n = strlen(str);
-    permute(str, 0, n-1);
-
-    // check permutations against dictionary
+    int fd; // file descriptor, as opposed to file pointer, ahem
+    size_t sz = (size_t)n;
+    const char *dict;
+    
+    // slurp local DICTIONARY into memory
     //
+    // FILE *fp = fopen(DICTIONARY, "r");
+    fd = open(DICTIONARY, O_RDONLY);
 
-    // ideally, slurp dictionary into memory
+    // if (fp != NULL) {
+    if (fd > 0 ) {
+	// dict = mmap(NULL, MAX_DICT_SIZE, PROT_READ, MAP_FILE, fp, 0);
+	dict = mmap(NULL, MAX_DICT_SIZE, PROT_READ, MAP_FILE, fd, 0);
+	if (dict == MAP_FAILED) { handle_error("mmap"); }
+    }
+
+    // fclose(fp);
+    close(fd);
+    
+    // check permutations against DICTIONARY
     //
+    permute(str, 0, n-1, dict, sz);
+
+    // release memory
+    //
+    munmap(&dict, MAX_DICT_SIZE);
     
     // count number of matches and exit on absolute value of (num - 1)
     //
